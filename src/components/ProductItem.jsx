ff2b8c86fa0fbc67e4a332b3c873723b37bb1f6e
balexgtx <balexgtx@gmail.com>
2022-05-24 16:33:47 -0600

    first commit
    First Commit

---
+++ src/components/ProductItem.jsx
@@ -0,0 +1,33 @@
+import React, { useState } from "react";
+import "@styles/ProductItem.scss";
+
+import addToCart from "@icons/bt_add_to_cart.svg";
+
+const ProductItem = () => {
+  const [cart, setCart] = useState([]);
+
+  const HandleClick = () => {
+    setCart([]);
+  };
+
+  return (
+    <div className="ProductItem">
+      <img
+        src="https://images.pexels.com/photos/276517/pexels-photo-276517.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=650&w=940"
+        alt=""
+      />
+      <div className="product-info">
+        <div>
+          <p>$120,00</p>
+          <p>Bike</p>
+        </div>
+
+        <figure onClick={HandleClick}>
+          <img src={addToCart} alt="" />
+        </figure>
+      </div>
+    </div>
+  );
+};
+
+export default ProductItem;
